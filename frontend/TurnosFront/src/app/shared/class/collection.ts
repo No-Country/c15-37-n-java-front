f1b7ea6abaf40ca0076e69ad4a9a1d8cb7c7fbac
Daniel Refchke <xshade@gmail.com>
2023-12-08 22:30:21 -0300

    Collection y model para la interaccion con el bak

---
+++ frontend/TurnosFront/src/app/shared/class/collection.ts
@@ -0,0 +1,48 @@
+import { Model } from './model';
+
+export class Collection<T> extends Array<T> {
+  model: any;
+  parentAtt: string;
+  parentAttValue: string;
+  static controler:any;
+
+  constructor(T, private _url:string="") {
+    super();
+    this.model = T;
+  }
+
+  public fetch():void{
+    
+  }
+
+  public sinc():void{
+
+  }
+
+  public get url():string{
+    return this._url;
+  }
+
+  public set url(u:string){
+    this._url = u;
+  }
+
+  public undoAll():void{
+    this.forEach((elemento:any)=>{
+      elemento.revert();
+    })
+  }
+
+  public parse(elems: any,ctrl:any=null): void {
+    if(this.length > 0){
+      this.splice(0,this.length);
+    }
+    let self = this;
+    elems.forEach((elem:any) => {
+      const modelInstance = new self.model(elem);
+      modelInstance.loaded();
+      modelInstance.fetch(ctrl)
+      this.push(modelInstance);
+    });
+  }
+}
---
+++ frontend/TurnosFront/src/app/shared/class/model.ts
@@ -0,0 +1,91 @@
+import { retry } from "rxjs";
+
+interface JsonData {
+  [key: string]: any; 
+}
+
+export class Model {
+  prev: JsonData;
+  attr: JsonData;
+  idAttr: string;
+  private _isNew: boolean;
+  private _isDeleted:boolean;
+
+  constructor(attr: {}, idAttr: string = "id") {
+    this.attr = attr;
+    this.prev = {};
+    this.idAttr = idAttr;
+    this._isNew = true;
+    this._isDeleted = false;
+  }
+
+  loaded(): void {
+    this._isNew = false;
+    this.prev = {};
+  }
+
+  get isDeleted():boolean{
+    return this._isDeleted;
+  }
+
+  delete(){
+    this._isDeleted=true;
+  }
+
+  unDelete(){
+    this._isDeleted = false;
+  }
+
+  serialize(): {} {
+    return this.attr;
+  }
+
+  unserialize(attr: {}):void{
+    this.attr = attr;
+    this.loaded();
+  };
+
+  toJson(): string {
+    return JSON.stringify(this.attr);
+  }
+
+  public get isChanged(): boolean {
+    //console.log((Object.keys(this.prev).length > 0));
+    
+    return Object.keys(this.prev).length > 0;
+  }
+
+  public get isNew():boolean{
+    return this._isNew;
+  }
+
+  revert() {
+    for (const field in this.prev) {
+      this.attr[field] = this.prev[field];
+    }
+    this.prev = {};
+    this._isDeleted = false;
+  }
+
+  confirm() {
+    this.prev = {};
+  }
+
+  setValue(field: string, value: any) {
+    //console.log(field);
+    this.change(field);
+    this.attr[field] = value;
+    //console.log(this.attr[field]);
+  }
+
+  getValue(field: string) {
+    return this.attr[field];
+  }
+
+  change(field: string) {
+    this.prev[field] = this.attr[field];
+    //console.log(this.prev[field]);
+  }
+
+  fetch(ctrl: any = null): void {}
+}
