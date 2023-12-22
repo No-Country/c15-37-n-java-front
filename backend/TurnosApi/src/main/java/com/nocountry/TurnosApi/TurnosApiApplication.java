03c01f156db6e2fcacc368f66471abd69b320b64
Luis Fernando González Avila <halo_paranoid@hotmail.com>
2023-12-21 20:25:28 -0600

    test2

--- backend/TurnosApi/src/main/java/com/nocountry/TurnosApi/TurnosApiApplication.java
+++ backend/TurnosApi/src/main/java/com/nocountry/TurnosApi/TurnosApiApplication.java
@@ -3,10 +3,9 @@ package com.nocountry.TurnosApi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+@SpringBootApplication
 @EntityScan(basePackages = "com.nocountry.TurnosApi.model")
 @EnableJpaRepositories(basePackages = "com.nocountry.TurnosApi.repository")
 public class TurnosApiApplication {
